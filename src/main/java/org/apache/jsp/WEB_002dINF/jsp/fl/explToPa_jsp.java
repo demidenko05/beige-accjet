199ca7df7d2cd86480fdbc705fe4c4d4f963e172
Yury Demidenko <demidenko05@gmail.com>
2020-11-04 13:26:23 +0300

    fixed payment filter, BC1.67

--- src/main/java/org/apache/jsp/WEB_002dINF/jsp/fl/explToPa_jsp.java
+++ src/main/java/org/apache/jsp/WEB_002dINF/jsp/fl/explToPa_jsp.java
@@ -1131,7 +1131,7 @@ public final class explToPa_jsp extends org.apache.jasper.runtime.HttpJspBase
     // /WEB-INF/jsp/fl/explToPa.jsp(45,0) name = var type = java.lang.String reqTime = false required = false fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
     _jspx_th_c_005fset_005f32.setVar("valLt100");
     // /WEB-INF/jsp/fl/explToPa.jsp(45,0) name = value type = javax.el.ValueExpression reqTime = true required = false fragment = false deferredValue = true expectedTypeName = java.lang.Object deferredMethod = false methodSignature = null
-    _jspx_th_c_005fset_005f32.setValue(new org.apache.jasper.el.JspValueExpression("/WEB-INF/jsp/fl/explToPa.jsp(45,0) '${invNm}.TOT 0 and TOPA/${invNm}.TOT lt 1'",_jsp_getExpressionFactory().createValueExpression(_jspx_page_context.getELContext(),"${invNm}.TOT 0 and TOPA/${invNm}.TOT lt 1",java.lang.Object.class)).getValue(_jspx_page_context.getELContext()));
+    _jspx_th_c_005fset_005f32.setValue(new org.apache.jasper.el.JspValueExpression("/WEB-INF/jsp/fl/explToPa.jsp(45,0) '${invNm}.TOT gt 0 and TOPA/${invNm}.TOT lt 1'",_jsp_getExpressionFactory().createValueExpression(_jspx_page_context.getELContext(),"${invNm}.TOT gt 0 and TOPA/${invNm}.TOT lt 1",java.lang.Object.class)).getValue(_jspx_page_context.getELContext()));
     int _jspx_eval_c_005fset_005f32 = _jspx_th_c_005fset_005f32.doStartTag();
     if (_jspx_th_c_005fset_005f32.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
       _005fjspx_005ftagPool_005fc_005fset_0026_005fvar_005fvalue_005fnobody.reuse(_jspx_th_c_005fset_005f32);
